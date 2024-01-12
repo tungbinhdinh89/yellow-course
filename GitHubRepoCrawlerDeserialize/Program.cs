3428ac3d34db08a224cd53c765ec73365137417c
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-12 18:55:28 +0700

    implement

---
+++ GitHubRepoCrawlerDeserialize/Program.cs
@@ -0,0 +1,21 @@
+﻿using GitHubRepoCrawler.Services;
+
+namespace GitHubRepoCrawlerDeserialize
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter your repo: ");
+            var search = Console.ReadLine()!;
+            var repos = await RepoCrawlerServiceDeserialize.SearchRepoGitHub(search, 10);
+
+            Console.Write("Enter your name of directory output: ");
+            var nameDir = Console.ReadLine()!;
+            GitHubWriteFileServiceDeserialize.WriteRepos(repos, nameDir);
+            Console.WriteLine("Save file success");
+
+            Console.ReadKey();
+        }
+    }
+}
