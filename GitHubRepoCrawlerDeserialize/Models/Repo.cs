232df204296e9df1195b008553bdb3dd26a8e2c2
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-12 18:53:50 +0700

    struct repo

---
+++ GitHubRepoCrawlerDeserialize/Models/Repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubRepoCrawler.Models
+{
+    public struct Repo
+    {
+        public string FullName { get; set; } 
+        public string Url { get; set; } 
+        public string Description { get; set; }
+        public string CreatedAt { get; set; } 
+        public int Star { get; set; }
+        public int Watch { get; set; }
+        public int Fork { get; set; }
+    }
+}
