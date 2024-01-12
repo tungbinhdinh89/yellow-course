a17bf9ae1fce40905e44b157e295f72ed1b69a44
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-12 13:28:15 +0700

    implement github service

---
+++ GithubRepoCrawler/Services/GithubWriteFileService.cs
@@ -0,0 +1,24 @@
+﻿using GithubRepoCrawler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubRepoCrawler.Services
+{
+    public class GitHubWriteFileService
+    {
+        public static void WriteRepos(List<Repo> repos, string folder)
+        {
+            //return [];
+            if (repos is null || repos.Count == 0)
+            {
+                throw new ArgumentException("No repo provided");
+            }
+
+            ArgumentException.ThrowIfNullOrWhiteSpace(folder);
+
+        }
+    }
+}
---
+++ GithubRepoCrawler/Services/RepoCrawlerService.cs
@@ -0,0 +1,30 @@
+﻿using GithubRepoCrawler.Models;
+
+namespace GithubRepoCrawler.Services
+{
+    public static class RepoCrawlerService
+    {
+        // Read file from url Repo
+        public static async Task<List<Repo>> SearchRepoGithub(string search, int take)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(search, nameof(search));
+
+            // request api service
+            var urlAPI = "https://api.github.com/search/repositories?q=";
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "C#");
+
+            var response = await httpClient.GetStringAsync(urlAPI+search);
+            if (string.IsNullOrWhiteSpace(response))
+            {
+                return [];
+            }
+
+            // read fille
+            var lines = response.Split("\n");
+            
+
+            return [];
+        }
+    }
+}
