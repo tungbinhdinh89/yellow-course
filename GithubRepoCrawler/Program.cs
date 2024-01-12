2809d46e763c690f815eaf3f148f6e8b67b1e904
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-12 13:58:44 +0700

    add search repo

---
+++ GithubRepoCrawler/Program.cs
@@ -0,0 +1,18 @@
+﻿using GithubRepoCrawler.Services;
+using GitHubRepoCrawler.Services;
+
+namespace GitHubRepoCrawler
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter your repo");
+            var search = Console.ReadLine()!;
+            var repos = await RepoCrawlerService.SearchRepoGithub(search, 10);
+                
+            GitHubWriteFileService.WriteRepos(repos, "download");
+               Console.ReadKey();
+        }
+    }
+}
--- GithubRepoCrawler/Services/RepoCrawlerService.cs
+++ GithubRepoCrawler/Services/RepoCrawlerService.cs
@@ -1,5 +1,4 @@
-﻿using GithubRepoCrawler.Models;
-
+﻿using GitHubRepoCrawler.Models;
 namespace GithubRepoCrawler.Services
 {
     public static class RepoCrawlerService
@@ -22,9 +21,92 @@ namespace GithubRepoCrawler.Services
 
             // read fille
             var lines = response.Split("\n");
-            
+            List<Repo> repos = [];
+            Repo repo = new();
 
-            return [];
+            foreach ( var line in lines)
+            {
+                if (GetValueFor("\"full_name\"", line, out var FullName))
+                {
+                    repo = new();
+                    repo.FullName = FullName;
+
+                    if (repos.Count < take)
+                    {
+                        repos.Add(repo);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                else if (GetValueFor("\"html_url\"", line, out var url))
+                {
+                    repo.Url = url;
+                }
+
+                else if (GetValueFor("\"description\"", line, out var desc))
+                {
+                    repo.Description = desc;
+                }
+
+                else if (GetValueFor("\"created_at\"", line, out var createdAt))
+                {
+                    repo.CreatedAt = createdAt;
+                }
+
+                else if (GetValueFor("\"stargazers_count\"", line, out var star))
+                {
+                    repo.Star = int.Parse(star);
+                }
+
+                else if (GetValueFor("\"watchers_count\"", line, out var watch))
+                {
+                    repo.Watch = int.Parse(watch);
+                }
+
+                else if (GetValueFor("\"forks_count\"", line, out var fork))
+                {
+                    repo.Fork = int.Parse(fork);
+                }
+            }
+            
+            return repos;   
+        }
+
+         static bool GetValueFor(string name, string line, out string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            value = "";
+
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return false;
+            }
+
+            if(line.Trim().StartsWith($"\"{name}\""))
+            {
+                value = GetValue(line);
+                return true;
+            }
+
+            return true;
+        }
+
+        static string GetValue(string line)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(line);
+
+            return line.Trim()
+                        .Trim(',')
+                        .Split(':')
+                        .Last()
+                        .Trim()
+                        .Trim('"')
+                        .Trim()
+                        ;
+
         }
     }
 }
