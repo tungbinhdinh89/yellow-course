ac3718ff15b9f5f6a3dcfb8802ce5bf4f3993dfd
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-12 13:27:08 +0700

    initial Repo struct

---
+++ GithubRepoCrawler/Models/Repo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubRepoCrawler.Models
+{
+    public class Repo
+    {
+        public string FullName { get; set; } = null!;
+        public string Url { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string CreatedAt { get; set; } = null!;
+        public int Star { get; set; }
+        public int Watch { get; set; }
+        public int Fork { get; set; }
+
+    }
+}
