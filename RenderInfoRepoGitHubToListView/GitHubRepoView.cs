ae5620c16a5251c79e5892a227c085f3b45d9c30
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-13 12:37:04 +0700

    add number to show repositories

--- RenderInfoRepoGitHubToListView/GitHubRepoView.cs
+++ RenderInfoRepoGitHubToListView/GitHubRepoView.cs
@@ -14,9 +14,43 @@ namespace RenderInfoRepoGitHubToListView
         private async void btnSearch_Click(object sender, EventArgs e)
         {
             var search = txtSearch.Text;
-            var repos = await crawlerRepoService.SearchRepoGitHub(search, 10);
-             RenderToListView(repos);
 
+            if (!string.IsNullOrWhiteSpace(search) && comboBox1.SelectedItem != null)
+            {
+                if (int.TryParse(comboBox1.SelectedItem.ToString(), out var selectedValue))
+                {
+
+                    var take = selectedValue;
+
+
+                    var repos = await crawlerRepoService.SearchRepoGitHub(search, take);
+                    if(repos is null || repos.Count == 0)
+                    {
+                        MessageBox.Show("Repository not found", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        //if(take > repos.Count)
+                        //{
+                        //    MessageBox.Show("Repository count is less than the your choose value", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        //    return;
+                        //}
+                        //else
+                        //{
+                            
+                        //}
+                        RenderToListView(repos);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid integer value from the ComboBox.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please provide a valid search term and select a value from the ComboBox.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void RenderToListView(List<Repo> repos)
