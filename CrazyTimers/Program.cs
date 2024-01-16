e7c98b5c4402a9de621682d0486a51b65fe176d0
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-16 08:36:04 +0700

    initial project

---
+++ CrazyTimers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrazyTimers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
