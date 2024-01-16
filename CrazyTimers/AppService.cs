550f9f4ecac79ee8e8296f2277cce854ddcb117e
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-16 11:07:06 +0700

    Seconds to Event

--- CrazyTimers/AppService.cs
+++ CrazyTimers/AppService.cs
@@ -14,31 +14,16 @@ namespace CrazyTimers
             Console.WriteLine("Version 1.0");
         }
 
-        public static int GetHourNumber(string input)
+        public static int GetNumber(string input)
         {
             Console.Write(input);
-            
             while (true)
             {
-                if(int.TryParse(Console.ReadLine(), out var value) && (value > 0 && value <24))
+                if (int.TryParse(Console.ReadLine(), out var value) )
                 {
                     return value;
                 }
-                Console.WriteLine("Invalid value");
-            }
-        }
-
-        public static int GetMinuteSecondNumber(string input)
-        {
-            Console.Write(input);
-
-            while (true)
-            {
-                if (int.TryParse(Console.ReadLine(), out var value) && (value > 0 && value < 60))
-                {
-                    return value;
-                }
-                Console.WriteLine("Invalid value");
+                //Console.WriteLine("Invalid value");
             }
         }
 
@@ -46,6 +31,28 @@ namespace CrazyTimers
         {
             Console.WriteLine($"The total number of seconds is : {hour * 60 * 60 + minute * 60 + second}") ;
         
+        }
+
+        public static void ConvertSecondToHour(int totalSeconds)
+        {
+            const int secondsInMinute = 60;
+            const int minutesInHour = 60;
+            Console.WriteLine($"This is equal to {totalSeconds / (minutesInHour * secondsInMinute)} hour, {(totalSeconds % (minutesInHour * secondsInMinute)) / secondsInMinute} minutes and {totalSeconds % secondsInMinute} seconds ");
+        }
+
+        public static long CalculateTotalSeconds(int hours, int days, int months, int years)
+        {
+            const int secondsInMinute = 60;
+            const int minutesInHour = 60;
+            const int hoursInDay = 24;
+            const int daysInMonth = 30; 
+
+            long totalSeconds = years * 365 * daysInMonth * hoursInDay * minutesInHour * secondsInMinute
+                              + months * daysInMonth * hoursInDay * minutesInHour * secondsInMinute
+                              + days * hoursInDay * minutesInHour * secondsInMinute
+                              + hours * minutesInHour * secondsInMinute;
+
+            return totalSeconds;
         }
     }
 }
--- CrazyTimers/Program.cs
+++ CrazyTimers/Program.cs
@@ -4,7 +4,81 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //AppService.GetFirstIntro();
+
+            //var hour = AppService.GetNumber("Enter the number of hours: ");
+            //while (!(hour > 0 && hour < 24))
+            //{
+            //    Console.WriteLine("The hours cannot be greater than 23 or less than 0.");
+            //    hour = AppService.GetNumber("Enter the number of hours: ");
+            //}
+
+            //var minutes = AppService.GetNumber("Enter the number of minutes: ");
+            //while (!(minutes > 0 && minutes < 60))
+            //{
+            //    Console.WriteLine("The minutes cannot be greater than 59 or less than 0.");
+            //    minutes = AppService.GetNumber("Enter the number of minutes: ");
+            //}
+
+            //var seconds = AppService.GetNumber("Enter the number of seconds: ");
+            //while (!(seconds > 0 && seconds < 60
+            //{
+            //    Console.WriteLine("The seconds cannot be greater than 59 or less than 0.");
+            //    seconds = AppService.GetNumber("Enter the number of seconds: ");
+            //}
+
+            //AppService.ConvertToSecond(hour, minutes, seconds);
+
+             //Reverse Countdown
+
+            //AppService.GetFirstIntro();
+
+            //var totalSeconds = AppService.GetNumber("Enter the number of total seconds: ");
+            //while (!(totalSeconds > 0))
+            //{
+            //    Console.WriteLine("The total seconds cannot be less than 0.");
+            //    totalSeconds = AppService.GetNumber("Enter the number of seconds: ");
+            //}
+
+            //AppService.ConvertSecondToHour(totalSeconds);
+
+
+            // Seconds to Event
+
+            AppService.GetFirstIntro();
+
+            var hour = AppService.GetNumber("Enter the number of hours: ");
+            while (!(hour > 0 && hour < 24))
+            {
+                Console.WriteLine("The hours cannot be greater than 23 or less than 0.");
+                hour = AppService.GetNumber("Enter the number of hours: ");
+            }
+
+            var days = AppService.GetNumber("Enter the number of days: ");
+            while (!(days > 0 && days < 365))
+            {
+                Console.WriteLine("The days cannot be greater than 365 or less than 0.");
+                days = AppService.GetNumber("Enter the number of hours: ");
+            }
+
+            var months = AppService.GetNumber("Enter the number of months: ");
+            while (!(months > 0 && months < 12))
+            {
+                Console.WriteLine("The months cannot be greater than 12 or less than 0.");
+                hour = AppService.GetNumber("Enter the number of hours: ");
+            }
+
+            var years = AppService.GetNumber("Enter the number of years: ");
+            while (!(years > 0 ))
+            {
+                Console.WriteLine("The years cannot be greater than 12 or less than 0.");
+                years = AppService.GetNumber("Enter the number of hours: ");
+            }
+
+            var totalSeconds = AppService.CalculateTotalSeconds(hour,days,months,years);
+
+            Console.WriteLine($"The total number of seconds is: {totalSeconds}");
+            Console.ReadKey();
         }
     }
 }
